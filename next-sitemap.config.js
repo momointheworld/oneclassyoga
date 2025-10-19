77fbf062162d4a28291fe7ffa6acf7dfea0eaeeb
lifenli <67099469+lifenli@users.noreply.github.com>
2025-10-19 16:42:52 +0800

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.oneclass.yoga',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  exclude: [
+    '/dashboard',      // exact route
+    '/dashboard/*',    // all nested routes inside dashboard
+    '/api/*',          
+  ],
+};
